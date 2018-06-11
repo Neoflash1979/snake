f0d9004e3707826a1e28179082248533f47add59
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2018-06-11 15:03:51 -0400

    Niveau 3

--- src/components/game.ts
+++ src/components/game.ts
@@ -1,23 +1,23 @@
-import Vue from "vue";
+import Vue from 'vue';
 import Vector from 'victor';
-import { Board } from "../board";
-import { Snake } from "../snake";
-import { GameLoop } from "../game-loop";
+import { Board } from '../board';
+import { Snake } from '../snake';
+import { GameLoop } from '../game-loop';
 
 export const Game = Vue.extend({
   template: `
   <canvas ref="game" width="600px" height="600px" style="border: 1px solid black"></canvas>
   `,
-  data: function(): {keyboardInput: string | undefined} {
+  data: function(): { keyboardInput: string | undefined } {
     return {
       keyboardInput: undefined
-    }
+    };
   },
   methods: {
-    keyDownListener: function (event: KeyboardEvent) {
-      this.onkey(event, event.key)
+    keyDownListener: function(event: KeyboardEvent) {
+      this.onkey(event, event.key);
     },
-    onkey: function (event: KeyboardEvent, key: KeyboardEvent['key']) {
+    onkey: function(event: KeyboardEvent, key: KeyboardEvent['key']) {
       switch (key) {
         case 'ArrowDown':
           this.keyboardInput = 'DOWN';
@@ -36,29 +36,34 @@ export const Game = Vue.extend({
           event.preventDefault();
           break;
       }
-    }    
+    }
   },
   mounted: function() {
-    const board = new Board(new Snake(new Vector(0, -1), [
-      new Vector(29, 29),
-      new Vector(29, 30),
-      new Vector(29, 31),
-      new Vector(29, 32),
-      new Vector(29, 33),
-      new Vector(29, 34)
-    ]));    
-    
-    const ctx = (<HTMLCanvasElement> this.$refs.game).getContext('2d') as CanvasRenderingContext2D;     
-    
+    const board = new Board(
+      new Snake(new Vector(0, -1), [
+        new Vector(29, 29),
+        new Vector(29, 30),
+        new Vector(29, 31),
+        new Vector(29, 32),
+        new Vector(29, 33),
+        new Vector(29, 34)
+      ])
+    );
+
+    const ctx = (<HTMLCanvasElement>this.$refs.game).getContext(
+      '2d'
+    ) as CanvasRenderingContext2D;
+
     document.addEventListener('keydown', this.keyDownListener);
-    
+
     const input = () => {
       if (this.keyboardInput) {
-        board.snake.setVelocity(this.keyboardInput as keyof typeof Snake.directions);
+        board.snake.setVelocity(this
+          .keyboardInput as keyof typeof Snake.directions);
         this.keyboardInput = undefined;
       }
     };
-    
+
     let previousSnake: Vector[];
     let currentSnake: Vector[];
     const update = (step: number) => {
@@ -66,26 +71,36 @@ export const Game = Vue.extend({
       board.update();
       currentSnake = board.snake.position;
     };
-    
+
+    const gameOverAnimation = {
+      fontSize: 0
+    };
     const render = (interp: number) => {
       ctx.clearRect(0, 0, 600, 600);
-      ctx.font = '18px serif';
       ctx.textAlign = 'center';
       ctx.textBaseline = 'middle';
+      ctx.font = '18px serif';
       //feed
-      ctx.fillText('*', board.feed.x / 3 * 30, board.feed.y / 3 * 30);
-    
+      ctx.fillText('*', (board.feed.x / 3) * 30, (board.feed.y / 3) * 30);
+
       //snake
       for (let i = 0; i < currentSnake.length - 1; i++) {
         ctx.fillText(
           'O',
-          currentSnake[i].x / 3 * 30 * interp + previousSnake[i].x / 3 * 30 * (1 - interp),
-          currentSnake[i].y / 3 * 30 * interp + previousSnake[i].y / 3 * 30 * (1 - interp)
+          (currentSnake[i].x / 3) * 30 * interp +
+            (previousSnake[i].x / 3) * 30 * (1 - interp),
+          (currentSnake[i].y / 3) * 30 * interp +
+            (previousSnake[i].y / 3) * 30 * (1 - interp)
         );
-      }  
-      
+      }
+      if (board.gameIsOver) {
+        gameOverAnimation.fontSize +=
+          gameOverAnimation.fontSize <= 60 ? 2 : 0;
+        ctx.font = `${gameOverAnimation.fontSize}px serif`;
+        ctx.fillText('GAME OVER', 300, 300);
+      }
     };
-    
+
     const loop = new GameLoop({
       updateTimeStep: 1000 / 60,
       fpsFilterStrength: 20,
@@ -94,10 +109,10 @@ export const Game = Vue.extend({
       update,
       render
     });
-    
+
     loop.start();
   },
   destroyed: function() {
     document.removeEventListener('keydown', this.keyDownListener);
   }
-})
+});
