23ce88793ccef919455c1d079bac0173af5cf751
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2018-06-13 18:03:52 -0400

    Fix bug in build process

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { app, BrowserWindow } from 'electron';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win: Electron.BrowserWindow | null;
